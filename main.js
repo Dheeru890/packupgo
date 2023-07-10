d78880631a8a2b3d3a7353cda495c9dae67e26ea
Dheeru890 <139106375+Dheeru890@users.noreply.github.com>
2023-07-10 12:21:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+const accordionItemHeaders = document.querySelectorAll(".accordion-item-header");
+
+accordionItemHeaders.forEach(accordionItemHeader => {
+  accordionItemHeader.addEventListener("click", event => {
+    
+    // Uncomment in case you only want to allow for the display of only one collapsed item at a time!
+    
+    // const currentlyActiveAccordionItemHeader = document.querySelector(".accordion-item-header.active");
+    // if(currentlyActiveAccordionItemHeader && currentlyActiveAccordionItemHeader!==accordionItemHeader) {
+    //   currentlyActiveAccordionItemHeader.classList.toggle("active");
+    //   currentlyActiveAccordionItemHeader.nextElementSibling.style.maxHeight = 0;
+    // }
+
+    accordionItemHeader.classList.toggle("active");
+    const accordionItemBody = accordionItemHeader.nextElementSibling;
+    if(accordionItemHeader.classList.contains("active")) {
+      accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+    }
+    else {
+      accordionItemBody.style.maxHeight = 0;
+    }
+    
+  });
+});
+function updateSlider() {
+    var slider = document.getElementById("image-slider");
+    var value = parseInt(slider.value);
+    var sliderContainer = document.getElementById("slider-container");
+    sliderContainer.style.transform = `translateX(-${value * 100}%)`;
+}
